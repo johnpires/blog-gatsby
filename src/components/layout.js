8f4da8a513c9bbba82f8bda6aea91b49c9357982
John Pires <j.pires@carteprogression.com>
2020-04-22 11:36:32 -0400

    Styling the header with styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,25 +38,58 @@ const navigationQuery = graphql`
   }
 `
 
-const NavLink = styled.div``;
+const NavLink = styled.div`
+  margin: auto 0;
+
+  a{
+    color: #FFFFFF;
+    padding: 0 16px;
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 16px;
+
+    &:hover{
+      color: orange;
+    }
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  background: #000000;
+  height: 66px;
+  padding: 0 16px;
+  box-sizing: border-box;
+`;
+
+const NavLinks = styled.div`
+  margin-left: auto;
+  display: flex;
+`;
 
 const Layout = ({ children }) => {  
 
   return (
     <>
-    <StaticQuery 
-      query={`${navigationQuery}`}
-      render={(data) => {
-        console.log(data);
-        return data.prismic.allNavigations.edges[0].node.navigation_links.map((link) => {
-          return (
-            <NavLink key={link.link._meta.uid}>
-              <Link to={`/${link.link._meta.uid}`}>{link.label}</Link>
-            </NavLink>
-          )
-        })
-      }}
-    />
+      <Header>
+        <NavLinks>
+          <StaticQuery
+            query={`${navigationQuery}`}
+            render={data => {
+              console.log(data)
+              return data.prismic.allNavigations.edges[0].node.navigation_links.map(
+                link => {
+                  return (
+                    <NavLink key={link.link._meta.uid}>
+                      <Link to={`/${link.link._meta.uid}`}>{link.label}</Link>
+                    </NavLink>
+                  )
+                }
+              )
+            }}
+          />
+        </NavLinks>
+      </Header>
       <Main>{children}</Main>
     </>
   )
