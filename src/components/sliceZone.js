495c7d9c916a7405e4fc18bcc10d14f578975a64
John Pires <j.pires@carteprogression.com>
2020-04-22 15:07:17 -0400

    Rendering the hero custom slice in Gatsby/React

---
+++ src/components/sliceZone.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hero from './hero';
+
+const SliceZone = ({body}) => {
+    console.log(body)
+    return(
+        <div>
+            {body.map((bodyContent, i)=>{
+                if(bodyContent.type === 'hero2'){
+                    return (
+                        <Hero
+                            title={bodyContent.primary.hero_title}
+                            content={bodyContent.primary.hero_content} 
+                            key={i}/>
+                    )
+                }else{
+                    return null;
+                }
+            })}
+        </div>
+    );
+};
+
+export default SliceZone;
